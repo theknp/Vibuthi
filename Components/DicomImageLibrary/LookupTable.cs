e9baef7185b924d422486891015b458d1b7dbd03
nkamalap <nagendra.kamalapuri@gmail.com>
2016-06-07 23:20:01 -0700

    fixed build issues

---
+++ Components/DicomImageLibrary/LookupTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicomImageLibrary
+{
+    public enum ImageBitsPerPixel { Eight, Sixteen, TwentyFour };
+    class LookupTable
+    {
+        private byte[] lookupTbl = new byte[256];
+
+        public LookupTable(int minimum, int maximum)
+        {
+            ComputeLookupTable(minimum, maximum);
+
+        }
+        public void ComputeLookupTable(int minimum, int maximum)
+        {
+            
+
+            if (maximum == 0)
+                maximum = 255;
+
+            int range = maximum - minimum;
+            if (range < 1) range = 1;
+            double factor = 255.0 / range;
+
+            for (int i = 0; i < 256; ++i)
+            {
+                if (i <= minimum)
+                    lookupTbl[i] = 0;
+                else if (i >= maximum)
+                    lookupTbl[i] = 255;
+                else
+                {
+                    lookupTbl[i] = (byte)((i - minimum) * factor);
+                }
+            }
+        }
+
+        public byte this[int key]
+        {
+            get
+            {
+
+                return lookupTbl[key];
+            }
+            
+        }
+        
+    }
+}
